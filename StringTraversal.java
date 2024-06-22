a8c60a3c6135ca06869c6a9f54c7433fc31ca255
skandhwa <113922787+skandhwa@users.noreply.github.com>
2024-06-22 11:46:57 +0530

    Commiting changes

---
+++ StringTraversal.java
@@ -0,0 +1,20 @@
+package MyFirstPractice;
+
+public class StringTraversal {
+
+	public static void main(String[] args) {
+		
+		String str="Selenium";
+        String []myval= {"Java","php","python"};
+        
+        for(String x:myval)
+        {
+        	System.out.println(x);
+        }
+        
+        
+        System.out.println(1+5+" "+"1+7+Selenium");
+
+	}
+
+}
